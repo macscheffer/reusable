e0bacb22ae1a39f18fb839df5b074f006080789a
macscheffer <43249742+macscheffer@users.noreply.github.com>
2019-03-14 00:44:34 -0400

    Update macs.py

--- macs.py
+++ macs.py
@@ -2,5 +2,15 @@
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import RobustScaler
 
 
+def macs_pca_feature(df, cols, pcs, clusters):
+
+     ‘’’
+      takes in a df. columns to reduce dimensions, how many dimensions to reduce to, and how many clusters to make.
+     ‘’’
+
+     
+     
