9226cb4cb682de7d4af5864857a69629ffaf918e
macscheffer <43249742+macscheffer@users.noreply.github.com>
2019-03-18 14:29:51 -0400

    Update macs.py

--- macs.py
+++ macs.py
@@ -32,29 +32,47 @@ def macs_pca_features(df, cols, pcs, clusters, prefix):
 
      return df
 
+##################################################################################################################################
 
+def date_to_features(col, df=df, drop=False):
+    
+    # a function to split a pandas datetime column into a different date columns (year, month, day, day of week, day of year)
+     datetime = pd.to_datetime(df[col])
+     df['datetime_year'] = datetime.dt.year
+     df['datetime_month'] = datetime.dt.month
+     df['datetime_dayofweek'] = datetime.dt.dayofweek
+    
+     df['datetime_dayofyear'] = datetime.dt.dayofyear
+     df['datetime_quarter'] = datetime.dt.quarter
+     df['datetime_weekofyear'] = datetime.dt.weekofyear
+    
+     if drop == True:
+          return df.drop(columns=col)
+     return df
+
+##################################################################################################################################
+
+def macs_labelEncode_meaningful(df, train, cols, target):
+    
+     # takes in a list of columns to encode in a linear fashion. 
+     
+     for col in cols:
+        
+          cur_encodes = pd.factorize(train.pivot_table(index=col, values=target).sort_values(target).index)[1].tolist()
+          fnl_encodes = pd.factorize(train.pivot_table(index=col, values=target).sort_values(target).index)[0].tolist()
+        
+          mapper_dic = dict(zip(cur_encodes, fnl_encodes))
+        
+          df['MeaningfulEnc_' + col] = df[col].map(mapper_dic)
+        
+     return df
 # a feature importance and performance evaluation function
      # will return a dataframe to analyze to find out what features are good at what
      
 # a create important boolean features function
      # it returns a dataframe with extra "dummy" features that combines feature and columns
      # for example is the house big and in a good neighborhood and has a lot of bathrooms
-     
-# a function to split a pandas datetime column into a different date columns (year, month, day, day of week, day of year)
-def date_to_features(col, df=df, drop=False):
-    
-    datetime = pd.to_datetime(df[col])
-    df['datetime_year'] = datetime.dt.year
-    df['datetime_month'] = datetime.dt.month
-    df['datetime_dayofweek'] = datetime.dt.dayofweek
-    
-    df['datetime_dayofyear'] = datetime.dt.dayofyear
-    df['datetime_quarter'] = datetime.dt.quarter
-    df['datetime_weekofyear'] = datetime.dt.weekofyear
-    
-    if drop == True:
-        return df.drop(columns=col)
-    return df
+
 
 # outlier detection function. can remove outlier rows or can add "is outlier" function. 
 
